050907a2fd9b9f08dc0deb8d7d52977a5d1028a4
gabipayan <gapayan26@gmail.com>
2025-06-10 19:51:54 -0600

    add React plugin to Vite configuration and configure GraphQL proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 3000,
     open: true,
@@ -14,7 +17,12 @@ export default defineConfig({
       '/auth': {
         target: 'http://localhost:3001',
         changeOrigin: true,
-        secure: false
+        secure: false,
+      },
+      '/graphql': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
