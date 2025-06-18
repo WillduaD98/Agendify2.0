d0bf2544cf9d3cbc83a7bc6efc28d6a8fe483550
[William Duarte] <[willi_dds3@hotmail.com]>
2025-06-17 20:37:12 -0600

    updating git ignore for clean reppo, also deleting extra files

--- server/dist/routes/api/user.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { getProfile, updateProfile } from '../../controllers/user.controller.js';
-const router = Router();
-router.get('/profile', getProfile);
-router.put('/profile', updateProfile);
-export { router as userRouter };
